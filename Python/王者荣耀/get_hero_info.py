cb03640449f9ad5eae2be86bebc75b961ab5af78
zzz-company <chengxiang@9ikandian.com>
2023-07-12 14:23:26 +0800

    获取英雄名称

--- Python/王者荣耀/get_hero_info.py
+++ Python/王者荣耀/get_hero_info.py
@@ -1,3 +1,4 @@
+import os.path
 import requests
 import json
 # --------------------- #
@@ -5,19 +6,24 @@ import json
 # 作者：cc
 # 功能：获取英雄的信息
 # --------------------- #
-headers = {
-    'Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    'Accept-Encoding': 'gzip, deflate, br'
-}
-url = 'https://pvp.qq.com/zlkdatasys/yuzhouzhan/list/heroList.json?t=1688565355373'
-response = requests.get(url,headers)
-json_str = json.loads(response.text)
-for i in json_str['yzzyxs_4880']:
-    address = i['zyyx_9680']
-    hero_name = i['yzzyxm_4588']
-    hero_id = i['yzzyxi_2602']
-    hero_type = i['yzzyxz_1918']
-    hero_title = i['yzzyxc_4613']
-    data = [address,hero_type,hero_id,hero_title,hero_name]
-    print(data)
+if not os.path.exists('英雄信息\\'):
+    os.mkdir('英雄信息\\')
+hero_name_file = '英雄信息\\hero_name.csv'
+
+def get_Data():
+    headers = {
+        'Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Accept-Encoding': 'gzip, deflate, br'
+    }
+    url = 'https://pvp.qq.com/zlkdatasys/yuzhouzhan/list/heroList.json?t=1688565355373'
+    response = requests.get(url,headers)
+    json_str = json.loads(response.text)
+    for i in json_str['yzzyxs_4880']:
+        hero_name = i['yzzyxm_4588']
+        with open(hero_name_file,'a',newline='') as file:
+            file.write(hero_name+'\n')-
+    print(f'已将所以英雄名字写入至 {hero_name_file}')
+
+if __name__ == '__main__':
+    get_Data()
